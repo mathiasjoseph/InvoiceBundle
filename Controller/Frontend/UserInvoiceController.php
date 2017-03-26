da55dd9880044365651b99a7b9a75d40a8932262
miky <mathias89100@gmail.com>
2017-03-27 01:34:22 +0200

    Merge branch 'master' of github.com:mikyprog/AdminBundle
    # Veuillez entrer un message de validation pour expliquer en quoi cette fusion est
    # nécessaire, surtout si cela fusionne une branche amont mise à jour dans une branche de sujet.
    #
    # Les lignes commençant par '#' seront ignorées, et un message vide
    # abandonne la validation.

---
+++ Controller/Frontend/UserInvoiceController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Miky\Bundle\InvoiceBundle\Controller\Frontend;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class UserInvoiceController extends Controller
+{
+
+    /**
+     * Reset user password
+     */
+    public function generatePDFAction()
+    {
+        $user = $this->container->get('adevis_customer_manager')->findUserByConfirmationToken($token);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with "confirmation token" does not exist for value "%s"', $token));
+        }
+
+        if (!$user->isPasswordRequestNonExpired($this->container->getParameter('fos_user.resetting.token_ttl'))) {
+            return new RedirectResponse($this->container->get('router')->generate('fos_user_resetting_request'));
+        }
+
+        $form = $this->container->get('fos_user.resetting.form');
+        $formHandler = $this->container->get('fos_user.resetting.form.handler');
+        $process = $formHandler->process($user);
+
+        if ($process) {
+            $this->setFlash('fos_user_success', 'resetting.flash.success');
+            $response = new RedirectResponse($this->getRedirectionUrl($user));
+            $this->authenticateUser($user, $response);
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('@MikyUser/Frontend/Resetting/reset.html.twig', array(
+            'token' => $token,
+            'form' => $form->createView(),
+        ));
+    }
+
+
+    /**
+     * @param string $action
+     * @param string $value
+     */
+    protected function setFlash($action, $value)
+    {
+        $this->container->get('session')->getFlashBag()->set($action, $value);
+    }
+
+}
