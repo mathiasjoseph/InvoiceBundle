935aad8b6dfe56fc5a666208e6cc0800de96aa8f
miky <mathias89100@gmail.com>
2017-05-28 14:06:13 +0200

    All

--- MikyInvoiceBundle.php
+++ MikyInvoiceBundle.php
@@ -2,8 +2,30 @@
 
 namespace Miky\Bundle\InvoiceBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MikyInvoiceBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $this->addRegisterMappingsPass($container);
+    }
+    /**
+     * @param ContainerBuilder $container
+     */
+    private function addRegisterMappingsPass(ContainerBuilder $container)
+    {
+        $mappings = array(
+            realpath(__DIR__.'/Resources/config/doctrine-mapping') => 'Miky\Bundle\InvoiceBundle\Model',
+        );
+        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
+            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings, array('miky_invoice.model_manager_name')));
+        }
+    }
 }
---
+++ Model/Invoice.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 24/05/17
+ * Time: 21:10
+ */
+
+namespace Miky\Bundle\InvoiceBundle\Model;
+
+
+class Invoice extends \Miky\Component\Invoice\Model\Invoice
+{
+
+}
